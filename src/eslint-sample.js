ec57c0fbbe3e40bf74b333d294e7fe946794d4ab
Ran Bar-Zik <ran@bar-zik.com>
2018-12-29 19:23:14 +0200

    initial commit

---
+++ src/eslint-sample.js
@@ -0,0 +1,13 @@
+/* This is eslint sample for bad code */ 
+
+const someConstant = 1;
+
+function ThisIsFunctionWithoutProperIndend() {
+    const a = 1;
+           return a;
+}
+
+
+function ThisIsFunctionWithBadArgument(arg1, arg2) {
+  return arg1;
+}
